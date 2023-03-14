3cfb2c7c4f25d6ff8c2c8e4b831c1f615058871b
Sertus <killianbellouard@gmail.com>
2023-03-14 22:53:57 +0100

    Create the structure of the application
    The application is structured around a singleton Application class, which runs an update-render loop
    
    This implementation uses SFML as game framework, and spdlog for logging

---
+++ include/Precompiled.h
@@ -0,0 +1,15 @@
+//
+// Created by Killian on 14/03/2023.
+//
+
+#pragma once
+
+#define _USE_MATH_DEFINES
+
+// Include these headers in all compiled files, doing this makes the compile-time go way lower
+// mainly because spdlog is a huge library that needs to be recompiled every time it is included
+#include <spdlog/spdlog.h>
+#include <SFML/Graphics.hpp>
+#include <SFML/Window.hpp>
+#include <SFML/System.hpp>
+#include <cmath>
